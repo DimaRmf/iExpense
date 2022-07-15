db35f6e1f12c5112e6d7d8002377bd9e43412a90
DimaRmf <->
2022-07-15 14:34:59 +0300

    Initial Commit

---
+++ iExpense/Expenseltment.swift
@@ -0,0 +1,14 @@
+//
+//  Expenseltment.swift
+//  iExpense
+//
+//  Created by Дима РМФ on 03.04.2022.
+//
+
+import Foundation
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
